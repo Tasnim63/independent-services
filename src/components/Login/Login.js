a1a591791392462d276de08086bdd67dc1d3317f
tasnim63 <tamanna202163@gmail.com>
2022-04-17 15:32:01 +0600

    regex commit

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css'
 import { useNavigate } from 'react-router-dom';
 import GoogleLogo from '../../assets/img/google.svg'
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import auth from '../../Firebase/Firesbase.init';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 
 const provider = new GoogleAuthProvider();
 const Login = () => {
     const navigate = useNavigate();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
     const googleAuth = () => {
 
         signInWithPopup(auth, provider)
@@ -20,24 +24,49 @@ const Login = () => {
 
             });
     }
-
-
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        hookError,
+    ] = useSignInWithEmailAndPassword(auth);
+    const handdleEmailChange = (e) => {
+        const emailRegex = /\S+@\S+\.\S+/;
+        const validEmail = emailRegex.test(e.target.value);
+        if (validEmail) {
+            setEmail(e.target.value);
+        } else {
+            setError('Invalid email !!')
+        }
+    }
+    const handdlePasswordChange = (e) => {
+        const passwordRegex = /.{6,}/;
+        const validPassword = passwordRegex.test(e.target.value);
+        console.log(validPassword);
+        // setPassword(e.target.value);
+    }
+    const handdleLogin = (e) => {
+        e.preventDefault();
+        signInWithEmailAndPassword(email, password);
+    }
 
     return (
         <div className='auth-form-container '>
             <div className='auth-form'>
                 <h1>Login</h1>
-                <form>
+                <form onSubmit={handdleLogin}>
                     <div className='input-field'>
                         <label htmlFor='email'>Email</label>
                         <div className='input-wrapper'>
-                            <input type='text' name='email' id='email' />
+                            <input type='text' name='email' onChange={handdleEmailChange} />
+
                         </div>
+                        {error && <p>{error}</p>}
                     </div>
                     <div className='input-field'>
                         <label htmlFor='password'>Password</label>
                         <div className='input-wrapper'>
-                            <input type='password' name='password' id='password' />
+                            <input type='password' name='password' onChange={handdlePasswordChange} />
                         </div>
                     </div>
                     <button type='submit' className='auth-form-submit'>
