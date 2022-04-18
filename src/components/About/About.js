75ff50e77f2a0172a2118e2a6a753405cf7337ce
tasnim63 <tamanna202163@gmail.com>
2022-04-18 12:52:44 +0600

    readme.md commted

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,9 +4,9 @@ import './About.css'
 
 export const About = () => {
     return (
-        <div className='container m-5 p-5'>
+        <div className='container-fluid mt-5 p-5'>
             <h1 className='name m-5 text-center'>Tamanna Akter</h1>
-            <p className='describetion'>I want to be full stack web Developer.For That i am still working and I spend 10 hour per day.I practice module and attended conceptual regularly.On other hand if I can not understand any topic then search in the google.If I can cont find any soluation then I create a post. </p>
+            <p className='describetion'>I want to be full stack web Developer.For That i am still working and I spend 10 hour per day.I practice module and attended conceptual sessions regularly.On other hand if I can not understand any topic then search in the google.If I can cont find any soluation then I create a post. </p>
         </div>
     )
 }
