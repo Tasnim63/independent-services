0aed659626dd661aa0dab6a1c94c11d6b5e2b3d0
tasnim63 <tamanna202163@gmail.com>
2022-04-18 12:38:59 +0600

    about me

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import { AiOutlineCopyrightCircle } from 'react-icons/ai';
-import { BsHeartFill, BsHeart } from "react-icons/bs";
+import { BsFacebook, BsInstagram } from "react-icons/bs";
+import { FiTwitter } from "react-icons/fi";
 import { Link } from 'react-router-dom';
 import './Footer.css'
 const Footer = () => {
     const today = new Date();
     const year = today.getFullYear();
     return (
-        <div className='container-fluid bg-dark'>
-            <div className=' row   p-5 '>
+        <div className='container-fluid bg-dark p-5'>
+            <div className='row p-3'>
 
-                <div className=' d-flex ps-5'>
-                    <div> <p className=' text-white'>More About <Link to='/About'>Click Here</Link></p></div>
-                    <div> <p className='footer-des ps-5'><AiOutlineCopyrightCircle></AiOutlineCopyrightCircle> Photographer Services. All Right Reserved. {year}</p></div>
-                    <div><p className=' text-white'><BsHeartFill /></p></div>
+                <div className='d-flex ps-5'>
+                    <div> <p className=' text-white'>More About <Link to='/About'>Click Here</Link></p>
+                    </div>
+
+                    <div> <p className='footer-des ps-5'><AiOutlineCopyrightCircle></AiOutlineCopyrightCircle> Photographer Services. All Right Reserved. {year}</p>
+                    </div>
+                    <div className='ps-5 pe-2 ms-5'><p className=' text-white'>Contact us <span className='ms-3'> <BsFacebook /> </span>  <span className='ms-3'> <BsInstagram /></span><span className='ms-3'> <FiTwitter /></span> </p>
+                    </div>
+
+
                 </div>
+
 
             </div>
         </div>
